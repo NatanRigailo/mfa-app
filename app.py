90afec686064385bc6fc3d7f7656a84ff826457d
Natan Rigailo <natan.rigailo@outlook.com>
2026-04-19 02:18:00 -0300

    adding the first phase of ci/cd pipeline

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=5000)  # nosec B104 — bind all interfaces é esperado em container
