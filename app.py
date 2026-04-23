48f94e00c47380c51ac1f0a3e8540208fe3d73f5
Natan Rigailo <natan.rigailo@outlook.com>
2026-04-23 00:05:18 -0300

    deploy on render a demo page

--- app.py
+++ app.py
@@ -270,4 +270,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)  # nosec B104
+    port = int(os.getenv("PORT", 5000))
+    serve(app, host="0.0.0.0", port=port)  # nosec B104
